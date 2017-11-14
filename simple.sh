fd0a445638fb04c0397f3fc60779172a59779b59
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-11-13 20:03:01 -0500

    modify setup script

--- simple.sh
+++ simple.sh
@@ -1,3 +1,10 @@
 ./ktruss datasets/facebook_wcc.txt datasets/facebook_wcc.txt > log_facebook &
-./ktruss datasets/wiki_wcc.txt datasets/testcases/regular/wiki_testcases.txt > log_wiki &
-./ktruss datasets/Livejournal_wcc.txt datasets/testcases/regular/Livejournal_testcases.txt > log_lj &
+./ktruss datasets/bio_wcc.txt datasets/bio_wcc.txt > log_bio &
+./ktruss datasets/hollywood_wcc.txt datasets/hollywood_wcc.txt > log_hollywood &
+./ktruss datasets/Livejournal_wcc.txt datasets/Livejournal_wcc.txt > log_Livejournal &
+./ktruss datasets/skitter_wcc.txt datasets/skitter_wcc.txt > log_skitter &
+./ktruss datasets/wiki_wcc.txt datasets/wiki_wcc.txt > log_wiki &
+./ktruss datasets/orkut_wcc.txt datasets/orkut_wcc.txt > log_orkut &
+./ktruss datasets/sinaweibo_wcc.txt datasets/sinaweibo_wcc.txt > log_sinaweibo &
+./ktruss datasets/webuk_wcc.txt datasets/webuk_wcc.txt > log_webuk &
+./ktruss datasets/friendster_wcc.txt datasets/friendster_wcc.txt > log_friendster &
