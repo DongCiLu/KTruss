975487d1d1c8d71c7a022771f3cd62959388701b
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-11-13 20:04:34 -0500

    modify setup script

--- simple.sh
+++ simple.sh
@@ -1,10 +1,11 @@
 ./ktruss datasets/facebook_wcc.txt datasets/facebook_wcc.txt > log_facebook &
-./ktruss datasets/bio_wcc.txt datasets/bio_wcc.txt > log_bio &
-./ktruss datasets/hollywood_wcc.txt datasets/hollywood_wcc.txt > log_hollywood &
-./ktruss datasets/Livejournal_wcc.txt datasets/Livejournal_wcc.txt > log_Livejournal &
-./ktruss datasets/skitter_wcc.txt datasets/skitter_wcc.txt > log_skitter &
 ./ktruss datasets/wiki_wcc.txt datasets/wiki_wcc.txt > log_wiki &
+./ktruss datasets/skitter_wcc.txt datasets/skitter_wcc.txt > log_skitter &
+./ktruss datasets/bio_wcc.txt datasets/bio_wcc.txt > log_bio &
+./ktruss datasets/Livejournal_wcc.txt datasets/Livejournal_wcc.txt > log_Livejournal &
+./ktruss datasets/hollywood_wcc.txt datasets/hollywood_wcc.txt > log_hollywood &
 ./ktruss datasets/orkut_wcc.txt datasets/orkut_wcc.txt > log_orkut &
 ./ktruss datasets/sinaweibo_wcc.txt datasets/sinaweibo_wcc.txt > log_sinaweibo &
+
 ./ktruss datasets/webuk_wcc.txt datasets/webuk_wcc.txt > log_webuk &
 ./ktruss datasets/friendster_wcc.txt datasets/friendster_wcc.txt > log_friendster &
