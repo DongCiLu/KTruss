91ce13c5f19e7259fec49de48831716e5ce5693d
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-11-20 16:02:15 -0500

    modify script

--- simple.sh
+++ simple.sh
@@ -1,11 +1,11 @@
-./main datasets/facebook_wcc.txt datasets/facebook_wcc.txt > log_facebook
-./main datasets/wiki_wcc.txt datasets/wiki_wcc.txt > log_wiki
-./main datasets/skitter_wcc.txt datasets/skitter_wcc.txt > log_skitter
-./main datasets/bio_wcc.txt datasets/bio_wcc.txt > log_bio
-./main datasets/Livejournal_wcc.txt datasets/Livejournal_wcc.txt > log_Livejournal
-./main datasets/hollywood_wcc.txt datasets/hollywood_wcc.txt > log_hollywood
-./main datasets/orkut_wcc.txt datasets/orkut_wcc.txt > log_orkut
-./main datasets/sinaweibo_wcc.txt datasets/sinaweibo_wcc.txt > log_sinaweibo
+./main index datasets/facebook_wcc.txt > log_facebook
+./main index datasets/wiki_wcc.txt > log_wiki
+./main index datasets/skitter_wcc.txt > log_skitter
+./main index datasets/Livejournal_wcc.txt > log_Livejournal
+./main index datasets/hollywood_wcc.txt > log_hollywood
+./main index datasets/orkut_wcc.txt > log_orkut
+./main index datasets/sinaweibo_wcc.txt > log_sinaweibo
 
-./main datasets/webuk_wcc.txt datasets/webuk_wcc.txt > log_webuk
-./main datasets/friendster_wcc.txt datasets/friendster_wcc.txt > log_friendster
+./main index datasets/webuk_wcc.txt > log_webuk
+./main index datasets/friendster_wcc.txt > log_friendster
+./main index datasets/bio_wcc.txt > log_bio
