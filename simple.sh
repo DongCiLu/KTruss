afc8a3556a92bcf3055ac0019094145aa10ff574
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-12-18 13:25:46 -0500

    generate new indices

--- simple.sh
+++ simple.sh
@@ -2,10 +2,9 @@
 ./main index datasets/wiki_wcc.txt > log_wiki
 ./main index datasets/skitter_wcc.txt > log_skitter
 ./main index datasets/Livejournal_wcc.txt > log_Livejournal
-./main index datasets/hollywood_wcc.txt > log_hollywood
+./main index datasets/baidu_wcc.txt > log_baidu
 ./main index datasets/orkut_wcc.txt > log_orkut
 ./main index datasets/sinaweibo_wcc.txt > log_sinaweibo
 
-./main index datasets/webuk_wcc.txt > log_webuk
-./main index datasets/friendster_wcc.txt > log_friendster
+./main index datasets/hollywood_wcc.txt > log_hollywood
 ./main index datasets/bio_wcc.txt > log_bio
