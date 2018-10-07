b72bbecb4070ba667ce2c7ce66e9f983582039e5
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2018-10-07 19:26:09 -0400

    modify script

--- setup_single.sh
+++ setup_single.sh
@@ -16,20 +16,20 @@ git clone https://github.com/snap-stanford/snap.git
 mv ~/snap ~/Snap
 mkdir ~/Snap/examples/ktruss
 ln -s /mydata ~/Snap/examples/ktruss/datasets
-ln -s ~/Ktruss/common.hpp ~/Snap/examples/ktruss/common.hpp
-ln -s ~/Ktruss/decomposer.hpp ~/Snap/examples/ktruss/decomposer.hpp
-ln -s ~/Ktruss/induced_graph.hpp ~/Snap/examples/ktruss/induced_graph.hpp
-ln -s ~/Ktruss/main.cpp ~/Snap/examples/ktruss/main.cpp
-ln -s ~/Ktruss/query.hpp ~/Snap/examples/ktruss/query.hpp
-ln -s ~/Ktruss/tcp_index.hpp ~/Snap/examples/ktruss/tcp_index.hpp
-ln -s ~/Ktruss/testcase.hpp ~/Snap/examples/ktruss/testcase.hpp
-ln -s ~/Ktruss/tree_index.hpp ~/Snap/examples/ktruss/tree_index.hpp
-ln -s ~/Ktruss/update.hpp ~/Snap/examples/ktruss/update.hpp
-ln -s ~/Ktruss/archiver.hpp ~/Snap/examples/ktruss/archiver.hpp
-ln -s ~/Ktruss/unittest.cpp ~/Snap/examples/ktruss/unittest.cpp
-ln -s ~/Ktruss/Makefile ~/Snap/examples/ktruss/Makefile
-ln -s ~/Ktruss/Makefile.ex ~/Snap/examples/ktruss/Makefile.ex
-ln -s ~/Ktruss/simple.sh ~/Snap/examples/ktruss/simple.sh
+ln -s ~/KTruss/common.hpp ~/Snap/examples/ktruss/common.hpp
+ln -s ~/KTruss/decomposer.hpp ~/Snap/examples/ktruss/decomposer.hpp
+ln -s ~/KTruss/induced_graph.hpp ~/Snap/examples/ktruss/induced_graph.hpp
+ln -s ~/KTruss/main.cpp ~/Snap/examples/ktruss/main.cpp
+ln -s ~/KTruss/query.hpp ~/Snap/examples/ktruss/query.hpp
+ln -s ~/KTruss/tcp_index.hpp ~/Snap/examples/ktruss/tcp_index.hpp
+ln -s ~/KTruss/testcase.hpp ~/Snap/examples/ktruss/testcase.hpp
+ln -s ~/KTruss/tree_index.hpp ~/Snap/examples/ktruss/tree_index.hpp
+ln -s ~/KTruss/update.hpp ~/Snap/examples/ktruss/update.hpp
+ln -s ~/KTruss/archiver.hpp ~/Snap/examples/ktruss/archiver.hpp
+ln -s ~/KTruss/unittest.cpp ~/Snap/examples/ktruss/unittest.cpp
+ln -s ~/KTruss/Makefile ~/Snap/examples/ktruss/Makefile
+ln -s ~/KTruss/Makefile.ex ~/Snap/examples/ktruss/Makefile.ex
+ln -s ~/KTruss/simple.sh ~/Snap/examples/ktruss/simple.sh
 cd ~/Snap/examples/ktruss
 make
 
